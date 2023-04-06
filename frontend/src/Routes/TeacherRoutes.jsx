90cd15f0fd5d82093f0bd3792613442cd9643a7d
IhsanRasheed <ihsanrasheed@gmail.com>
2023-04-06 14:17:54 +0530

    name changed

--- frontend/src/Routes/TeacherRoutes.jsx
+++ frontend/src/Routes/TeacherRoutes.jsx
@@ -1,6 +1,6 @@
 import {Route, Routes} from 'react-router-dom'
 import Home from '../Pages/Teacher/Home'
-import TeacherVerification from '../Verification/TeacherVerifiction'
+import TeacherVerification from '../Verification/TeacherVerification'
 
 
 function TeacherRoutes() {
