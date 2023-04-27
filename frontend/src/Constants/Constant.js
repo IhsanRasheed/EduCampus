9660f3a07257372777eb74b1d73aeb2deba3d680
IhsanRasheed <ihsanrasheed@gmail.com>
2023-04-27 10:03:24 +0530

    constants

--- frontend/src/Constants/Constant.js
+++ frontend/src/Constants/Constant.js
@@ -1,5 +1,5 @@
-export const BASEURL='http://localhost:4000/backend'
-// export const BASEURL='https://educampus.online/backend'
+// export const BASEURL='http://localhost:4000/backend'
+export const BASEURL='https://educampus.online/backend'
 
 
 export const razorpaykeyId = 'rzp_test_B2KOTBQUPIMpnb'
